04157515bcd241a0e18b88da19c77245b0087a3e
Muzakhi_Achmad <zaky39415@gmail.com>
2026-03-03 04:03:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -148,8 +148,15 @@ function logout(){
   localStorage.removeItem("login");
   location.reload();
 }
-   
-  updateDashboard();
+// INIT MAP
+var map = L.map('map').setView([-6.2, 106.8], 13);
+
+// Tile map (gratis)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:'© OpenStreetMap'
+}).addTo(map);   
+
+   updateDashboard();
 }
 
 // load awal
